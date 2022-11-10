b677c9a4e3eb060260c64012a8116f3eb6ea75de
Merto2706 <mertali27062001@hotmail.com>
2022-11-10 04:05:27 +0100

    Final solution

--- TypeScript.js
+++ TypeScript.js
@@ -4,41 +4,66 @@ function genTxt() {
     var ec = String(document.getElementById('ec').value);
     document.getElementById("t1").innerHTML = "Exchange rate " + c2 + "/" + c1 + " = " + ec;
 }
-function generateTable() {
-    // creates a <table> element and a <tbody> element
-    var tbl = document.createElement("table");
-    var tblhead = document.createElement("thead");
-    var tblBody = document.createElement("tbody");
-    // creating all cells
-    for (var i = 0; i < 30; i++) {
-        // creates a table row
-        var row = document.createElement("tr");
-        for (var j = 0; j < 2; j++) {
-            // Create a <td> element and a text node, make the text
-            // node the contents of the <td>, and put the <td> at
-            // the end of the table row
-            var cell = document.createElement("td");
-            var cellText = document.createTextNode("cell in row ".concat(i, ", column ").concat(j));
-            cell.appendChild(cellText);
-            row.appendChild(cell);
+function del() {
+    document.getElementById("tbl1").innerHTML = "";
+}
+function gentbl() {
+    var c1 = String(document.getElementById('c1').value);
+    var c2 = String(document.getElementById('c2').value);
+    var ec = Number(document.getElementById('ec').value);
+    var table = document.getElementById("tbl1");
+    var header = table.createTHead();
+    var rowh = header.insertRow(0);
+    var cellhl = rowh.insertCell(0);
+    var cellhr = rowh.insertCell(1);
+    cellhl.innerHTML = c1;
+    cellhr.innerHTML = c2;
+    var c = 2;
+    for (var i = 1; i < 29; i++) {
+        var row = table.insertRow(i);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        if (i < 11) {
+            cell1.innerHTML = i;
+            cell2.innerHTML = (i * ec).toFixed(2);
+            cell1.style.background = "aquamarine"; //falsch?
+            cell2.style.background = "aquamarine"; //falsch?
         }
-        // add the row to the end of the table body
-        tblBody.appendChild(row);
+        else if (10 < i && i < 20) {
+            cell1.innerHTML = ((i - 9) * 10);
+            cell2.innerHTML = (((i - 9) * 10) * ec).toFixed(2);
+            cell1.style.background = "blueviolet";
+            cell2.style.background = "blueviolet";
+        }
+        else {
+            cell1.innerHTML = (c * 100);
+            cell2.innerHTML = ((c * 100) * ec).toFixed(2);
+            cell1.style.background = "goldenrod";
+            cell2.style.background = "goldenrod";
+            c++;
+        }
     }
-    // put the <tbody> in the <table>
-    tbl.appendChild(tblBody);
-    // appends <table> into <body>
-    document.body.appendChild(tbl);
-    // sets the border attribute of tbl to '2'
-    tbl.setAttribute("border", "1");
+}
+function textunder() {
+    document.getElementById("t2").innerHTML = "Currency table";
+}
+function swap() {
+    var c1 = String(document.getElementById('c1').value);
+    var zwi = c1;
+    document.getElementById("c1").value = document.getElementById('c2').value;
+    document.getElementById('c2').value = zwi;
+    document.getElementById("ec").value = 1 / document.getElementById("ec").value;
 }
 document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("button1").addEventListener("click", function (event) {
         event.preventDefault();
+        swap();
     });
     document.getElementById("button2").addEventListener("click", function (event) {
         event.preventDefault();
+        del();
         genTxt();
-        generateTable();
+        gentbl();
+        textunder();
     });
 });
--- TypeScript.ts
+++ TypeScript.ts
@@ -1,57 +1,83 @@
-function genTxt(){
+function genTxt() {
     let c1: String = String((document.getElementById('c1') as HTMLInputElement).value);
     let c2: String = String((document.getElementById('c2') as HTMLInputElement).value);
     let ec: String = String((document.getElementById('ec') as HTMLInputElement).value);
-    document.getElementById("t1").innerHTML = "Exchange rate " + c2 + "/" + c1 + " = " + ec;
 
+    document.getElementById("t1").innerHTML = "Exchange rate " + c2 + "/" + c1 + " = " + ec;
 }
 
-function generateTable() {
-    // creates a <table> element and a <tbody> element
-    const tbl = document.createElement("table");
-    const tblhead = document.createElement("thead");
-    const tblBody = document.createElement("tbody");
+function del() {
+    document.getElementById("tbl1").innerHTML = "";
+}
 
+function gentbl() {
 
-    // creating all cells
-    for (let i = 0; i < 30; i++) {
-        // creates a table row
-        const row = document.createElement("tr");
+    let c1: String = String((document.getElementById('c1') as HTMLInputElement).value);
+    let c2: String = String((document.getElementById('c2') as HTMLInputElement).value);
+    let ec: number = Number((document.getElementById('ec') as HTMLInputElement).value);
 
-        for (let j = 0; j < 2; j++) {
-            // Create a <td> element and a text node, make the text
-            // node the contents of the <td>, and put the <td> at
-            // the end of the table row
-            const cell = document.createElement("td");
-            const cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
-            cell.appendChild(cellText);
-            row.appendChild(cell);
+    let table = document.getElementById("tbl1") as HTMLElement;
+
+    let header = table.createTHead();
+    let rowh = header.insertRow(0);
+    let cellhl = rowh.insertCell(0);
+    let cellhr = rowh.insertCell(1);
+    cellhl.innerHTML = c1;
+    cellhr.innerHTML = c2;
+    let c = 2;
+
+    for (let i = 1; i < 29; i++) {
+        let row = table.insertRow(i);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+
+        if (i < 11) {
+            cell1.innerHTML = i;
+            cell2.innerHTML = (i * ec).toFixed(2);
+            cell1.style.background = "aquamarine";  //falsch?
+            cell2.style.background = "aquamarine";  //falsch?
+
+        } else if (10 < i && i < 20) {
+            cell1.innerHTML = ((i - 9) * 10);
+            cell2.innerHTML = (((i - 9) * 10) * ec).toFixed(2);
+            cell1.style.background = "blueviolet";
+            cell2.style.background = "blueviolet";
+        } else {
+            cell1.innerHTML = (c * 100);
+            cell2.innerHTML = ((c * 100) * ec).toFixed(2);
+            cell1.style.background = "goldenrod";
+            cell2.style.background = "goldenrod";
+            c++;
         }
-
-        // add the row to the end of the table body
-        tblBody.appendChild(row);
     }
 
-    // put the <tbody> in the <table>
-    tbl.appendChild(tblBody);
-    // appends <table> into <body>
-    document.body.appendChild(tbl);
-    // sets the border attribute of tbl to '2'
-    tbl.setAttribute("border", "1");
+
+}
+
+function textunder() {
+    document.getElementById("t2").innerHTML = "Currency table";
+}
+
+function swap() {
+    let c1: String = String((document.getElementById('c1') as HTMLInputElement).value);
+    let zwi = c1;
+    document.getElementById("c1").value = document.getElementById('c2').value;
+    document.getElementById('c2').value = zwi;
+    document.getElementById("ec").value = 1 / document.getElementById("ec").value;
+
 }
 
 
-
-
-document.addEventListener("DOMContentLoaded",(event)=>{
-    document.getElementById("button1").addEventListener("click", (event)=>{
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("button1").addEventListener("click", (event) => {
         event.preventDefault();
+        swap();
     })
-
-    document.getElementById("button2").addEventListener("click", (event)=>{
+    document.getElementById("button2").addEventListener("click", (event) => {
         event.preventDefault();
+        del();
         genTxt();
-        generateTable();
+        gentbl();
+        textunder();
     })
 })
-
